95e55cf823337a5c4ce133293c17ed4c69d344fe
Manuel Neuhauser <manuel@rokatu.com>
2018-10-25 10:25:00 -0400

    Updates 2018-10-25

---
+++ src/pages/service-agreement.js
@@ -0,0 +1,99 @@
+import React from 'react'
+
+const ServiceAgreementPage = () => (
+  <main className="row">
+    <div className="col-sm-10 offset-sm-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+
+      <h2>Service Agreement</h2>
+
+      <p>
+        This document constitutes the Agreement between Strength &amp; Conditioning Outlet, LLC, hereafter &ldquo;CrossFit Secaucus&rdquo;, and the service recipient, hereafter &ldquo;Client&rdquo;.
+      </p>
+      <p>
+        <strong>Credit Card Authorization / EFT Request and Authorization</strong>:
+        Client hereby authorizes CrossFit Secaucus or its assigns to make periodic charges or withdrawals (&ldquo;EFT Authorization&rdquo;) from the account designated to pay the initial payment described above or the account entered into Client&rsquo;s online account or replacement account designated by Client and accepted by CrossFit Secaucus for payment of any and all fees, late charges, costs, expenses, or any other monies due to CrossFit Secaucus under the terms and conditions of this Agreement.
+      </p>
+      <p>
+        <strong>No Refunds</strong>:
+        All sales are final, and no refunds are given. Refunds are not given for unused class credits.
+      </p>
+      <p>
+        <strong>Pricing Option Fees</strong>:
+        The monthly pricing option fee will be charged in advance of service automatically once per month to the payment method designated above. A 3% convenience fee applies to all credit card transactions. The first partial monthly pricing option fee will be pro-rated from the effective start date. Pricing options are non-transferable. Pricing option fees are subject to change upon 60-days notice.
+      </p>
+      <p>
+        <strong>Cancellation</strong>:
+        Pricing option renewals must be cancelled 30 days prior to the next renewal date, and cancellations are not retroactive. No exceptions. Relocation for employment, personal, health, or for any other reason still requires a 30 day notice. Cancellation requests must be emailed to trainers@crossfit-secaucus.com. No other form of transmittal, whether written or oral, will be permitted or honored. A confirmation email from a CrossFit Secaucus staff member is required for the cancellation to take effect. For example, if a request for cancellation is received on April 5, the pricing option will renew for May, but not for June.
+      </p>
+      <p>
+        <strong>Hold</strong>:
+        Pricing option renewals can be put on hold for one (1) to three (3) calendar months. No retroactive or partial month holds are permitted. No exceptions. The pricing option automatically renews at the end of the hold period. The first pricing option renewal immediately following the hold period cannot be modified or cancelled. Requests for holds must be submitted 14 days prior to the start of the desired hold period, and must be emailed to trainers@crossfit-secaucus.com. No other form of transmittal, whether written or oral, will be permitted or honored. A confirmation email from a CrossFit Secaucus staff member is required for the hold to take effect. For example, if a 2-month hold request is received on April 12, the pricing option will not renew in May and June. The pricing option will automatically renew in July.
+      </p>
+      <p>
+        <strong>Pricing Option Changes</strong>:
+        Requests for pricing option changes must be submitted 30 days prior to the start of the calendar month for which the new pricing option is desired, and must be emailed to trainers@crossfit-secaucus.com. No other form of transmittal, whether written or oral, will be permitted or honored. A confirmation email from a CrossFit Secaucus staff member is required for the pricing option change to take effect.
+      </p>
+      <p>
+        <strong>Class Credit Expirations</strong>:
+        Monthly pricing options include a set number of class credits which expire at the end of the calendar month. All remaining unused class credits expire with it, and will not roll over into the next calendar month. Non-monthly prepaid individual classes or classes on a punch card expire 3 months after the date of purchase. Prepaid personal training and fundamentals sessions expire after 12 months. Class credits, prepaid individual classes, personal training sessions and fundamentals sessions are non-tranferrable.
+      </p>
+      <p>
+        <strong>Account Balance</strong>:
+        Payments for product purchases (i.e. apparel, drinks, supplements) may be deferred by putting the charges on to an account tab. Client&rsquo;s full account tab balance consisting of all purchases will be collectively charged to the designated payment method at the end of the calendar month.
+      </p>
+      <p>
+        <strong>Late Payment / Declined Credit Card Fees</strong>:
+        A fee of $10 may be charged on any payment made past the due date or declined for insufficient funds. Meaning if Client is scheduled to be billed on the first of the month and payment is not made in full at that time, then an additional $10 will be assessed.
+      </p>
+      <p>
+        <strong>Class Sign-ups</strong>:
+        Client is required to register for class in advance via the scheduling system in order to be allowed to attend class. CrossFit Secaucus does not guarantee space in a class. Attendance in a class may be restricted to Client based on pricing option, type of class, size of class or for any other reason. A class credit is used to reserve your space in a class. Additional class credits can be purchased once all pre-paid class credits are utilized for the calendar month.
+      </p>
+      <p>
+        <strong>Class Cancellations</strong>:
+        Class sign-up cancellations can be made by Client via the scheduling system up to two (2) hours before class start with no class credit loss. Cancellations made less than two (2) hours before class start will result in the loss of the class credit.
+      </p>
+      <p>
+        <strong>Personal Training Appointment Cancellation</strong>:
+        Personal training sessions must be cancelled in writing at least 24 hours prior to the scheduled start time. Cancellation request must be emailed to trainers@crossfit-secaucus.com. A confirmation email from a CrossFit Secaucus staff member is required for the cancellation to be accepted. No other form of transmittal, whether written or oral, will be permitted or honored. Failure to abide by the cancellation policy will result in a session loss with no option to reschedule or receive a credit.
+      </p>
+      <p>
+        <strong>Photo/Video Release</strong>:
+        In consideration for the services provided by CrossFit Secaucus, Client hereby agrees to allow CrossFit Secaucus, its agents, officers, principals, employees and volunteers the use of any and all picture(s), film, and/or likeness of Client for advertising and teaching purposes. Client consents to being filmed, photographed and recorded by CrossFit Secaucus.
+      </p>
+      <p>
+        <strong>Lost, Stolen and Abandoned Articles</strong>:
+        CrossFit Secaucus assumes no responsibility for any personal property, whether lost, stolen or abandoned. Lost and found articles not claimed as well as any items deemed abandoned will be donated to charity or discarded at the sole discretion of CrossFit Secaucus. Further, CrossFit Secaucus is not responsible for items stolen under any circumstances. This includes theft from rental lockers with or without locks. Items found in any unpaid lockers will be discarded without notice.
+      </p>
+      <p>
+        <strong>Damages</strong>:
+        Client shall pay for any damages to CrossFit Secaucus property which results from the intentional, knowing, negligent, purposeful or reckless conduct of Client, Client&rsquo;s guest(s) or dependent child(ren). Client will be held financially responsible for equipment or property damage resulting from his/her negligent or reckless use, or intentional misuse of said equipment. CrossFit Secaucus and its agents shall have sole discretion as to the allocation of reasonable costs to the responsible party.
+      </p>
+      <p>
+        <strong>Personal Property Damage</strong>:
+        Client takes full financial responsibility for keeping his/her personal property safe and out of harm&rsquo;s way. CrossFit Secaucus is not responsible for any Client&rsquo;s personal items brought to the gym.
+      </p>
+      <p>
+        <strong>Personal Injury</strong>:
+        Client will be held financially responsible for personal injury that client may cause to themselves or others resulting from actions by Client, Client&rsquo;s guest(s) or dependent child(ren) whether such actions were intentional, knowing, negligent, purposeful or reckless.
+      </p>
+      <p>
+        <strong>Conduct</strong>:
+        CrossFit Secaucus is committed to the health, safety, and welfare of each of its clients and staff. As such, CrossFit Secaucus will not tolerate unreasonable, threatening, obscene, harassing, indecent, inappropriate, demeaning, disrespectful or illegal behavior. CrossFit Secaucus has the right to judge behavior and respond accordingly, including, but is not limited to, termination of service without advanced notice, warning or refund to any client engaging in behavior CrossFit Secaucus deems inappropriate, in its sole discretion.
+      </p>
+      <p>
+        <strong>Medical Advice</strong>:
+        Trainers and staff of CrossFit Secaucus are not medical professionals. Any and all advice sought and received is not intended to substitute for professional medical advice, diagnosis or treatment from a licensed medical professional. Client hereby acknowledges that any advice sought or provided shall not be relied upon in absence of or in conjunction with a professional medical opinion. Neither CrossFit Secaucus or its principals, officers, agents, employees, organizers, representatives, trainers/coaches, volunteers, whether individually or collectively shall be liable to Client or Client&rsquo;s successors, representatives, heirs, executors, assigns, or transferees for any advice mistakenly relied upon. Never disregard professional medical advice, or delay in seeking it, because of something that an owner, trainer, staff member, or any agent or purported agent of CrossFit Secaucus has stated. Never rely on information provided to Client by CrossFit Secaucus in place of seeking professional medical advice. CrossFit Secaucus is not responsible or liable for any advice, course of treatment, diagnosis or any other information that Client obtained through CrossFit Secaucus. Client is strongly encouraged to confer with Client&rsquo;s doctor or other medical expert(s) with regard to information provided or obtained through CrossFit Secaucus.
+      </p>
+      <p>
+        <strong>Change of Rules and/or Regulations</strong>:
+        CrossFit Secaucus reserves the right to add, change or remove rules, conditions of pricing options, opening and closing hours and the services and facilities offered by CrossFit Secaucus from time to time as it sees fit in its sole discretion.
+      </p>
+      <p>Client acknowledges that Client has read, understood and agrees with all terms and conditions of this agreement.</p>
+      <p>This Agreement as well as CrossFit Secaucus&rsquo;s Waiver of Liability constitute the entire agreement of the parties and no other agreement, guarantee or understanding exists between Client and CrossFit Secaucus. CrossFit Secaucus has made no express or implied warranties or representations other than those expressly set forth in this Agreement and Waiver of Liability to induce Client to enter into this Agreement. Any conflict between the &ldquo;original&rdquo; signed documents and any &ldquo;copy&rdquo; of the originals, shall be controlled by the &ldquo;original&rdquo;.</p>
+
+    </div>
+  </main>
+)
+
+export default ServiceAgreementPage
