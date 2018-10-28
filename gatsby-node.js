94c86a4d30120bd3da943dec7d3bcf3315bbe5fd
Manuel Neuhauser <manuel@rokatu.com>
2018-10-28 13:22:26 -0400

    Add redirects from old site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,32 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = ({ boundActionCreators }) => {
+  const { createRedirect } = boundActionCreators;
 
- // You can delete this file if you're not using it
+  [
+    ['/wp', '/index.html'],
+    ['/wp/about-us', '/index.html'],
+    ['/wp/classes/crossfit', '/index.html#schedule'],
+    ['/wp/classes/crossfit-fundamentals', '/get-started.html#fundamentals'],
+    ['/wp/classes/crossfit-personal-training', '/index.html#how-it-works'],
+    ['/wp/contact-us', '/index.html#contact'],
+    ['/wp/frequently-asked-questions', '/index.html#how-it-works'],
+    ['/wp/our-classes', '/index.html#schedule'],
+    ['/wp/policies', '/service-agreement'],
+    ['/wp/pricing', '/index.html#pricing'],
+    ['/wp/schedule', '/index.html#schedule'],
+    ['/wp/scheduling-guide', '/index.html#how-it-works'],
+  ].forEach((r) => {
+    createRedirect({
+      fromPath: r[0],
+      isPermanent: true,
+      redirectInBrowser: true,
+      toPath: r[1],
+    });
+
+    createRedirect({
+      fromPath: `${r[0]}/`,
+      isPermanent: true,
+      redirectInBrowser: true,
+      toPath: r[1],
+    });
+  });
+};
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Link from "gatsby-link"
+
+import Tile from '../components/Tile'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <main>
+    <section className="section section-basic">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-10 offset-sm-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+            <div className="text-center">
+              <h1>404 Page Not Found</h1>
+              <h3>This is not the web page you're looking for!</h3>
+              <Link to="/index.html#contact" className="btn btn-primary btn-lg">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section">
+      <div className="row no-gutters">
+        <Tile url="/img/athlete-3-center.jpg" className="d-none d-lg-block" />
+        <Tile url="/img/athletes-center.jpg" />
+        <Tile url="/img/athlete-3-right.jpg" className="d-none d-lg-block" />
+      </div>
+    </section>
+  </main>
 )
 
 export default NotFoundPage
