66e4078403f904230950741f4d3070076336c6cc
Manuel Neuhauser <manuel@rokatu.com>
2018-10-28 19:54:21 -0400

    Update favicons

--- src/html.js
+++ src/html.js
@@ -32,10 +32,16 @@ module.exports = class HTML extends React.Component {
           {this.props.headComponents}
           {css}
 
-          <link rel="shortcut icon" href="/img/icon/favicon.png" />
-          <link rel="apple-touch-icon" href="/img/icon/CrossFit-Secaucus.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/img/icon/CrossFit-Secaucus-72.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/img/icon/CrossFit-Secaucus-144.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/icon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/icon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/icon/favicon-16x16.png" />
+          <link rel="manifest" href="/img/icon/site.webmanifest" />
+          <link rel="mask-icon" href="/img/icon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/img/icon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="msapplication-config" content="/img/icon/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
+
 
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons|Montserrat" rel="stylesheet" />
 
