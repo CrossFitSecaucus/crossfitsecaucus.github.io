8099ae48e5ef5207b644bde8b8fa4219c5f39fdf
Manuel Neuhauser <manuel@rokatu.com>
2018-10-28 14:11:12 -0400

    Updates

---
+++ component---src-pages-404-js-710ce6788468a273f35f.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{216:function(e,n,t){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}n.__esModule=!0;var o=t(1),l=(u(o),function(){return window.location="/",null});n.default=l,e.exports=n.default}});
+//# sourceMappingURL=component---src-pages-404-js-710ce6788468a273f35f.js.map
