42dd9a37f085f9a1c493c2a9a10ab057bce086db
Manuel Neuhauser <manuel@rokatu.com>
2020-04-06 07:54:55 -0400

    Add links page

---
+++ src/pages/links.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Helmet } from "react-helmet"
+import Link from 'gatsby-link'
+
+const Button = ({label, to, isExternal = false, style = 'primary'}) => (
+  <div className="row" style={{margin: 15, width: '100%'}}>
+    <div className="col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+      {
+        isExternal ? (
+          <a href={to} className={`btn btn-${style} btn-lg btn-block`} style={{margin: 'auto'}}>
+            {label}<div className="ripple-container"></div>
+          </a>
+        ) : (
+          <Link to={to} className={`btn btn-${style} btn-lg btn-block`} style={{margin: 'auto'}}>
+            {label}
+          </Link>
+        )
+      }
+    </div>
+  </div>
+);
+
+const LinksPage = () => (
+  <main className="row">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Links - CrossFit Secaucus</title>
+      <meta name="description" content="Overview of recent links." />
+      <link rel="canonical" href="https://crossfit-secaucus.com/links" />
+      <meta property="og:title" content="Overview of recent links." />
+      <meta property="og:url" content="https://crossfit-secaucus.com/links/" />
+      <meta property="og:image" content="https://crossfit-secaucus.com/img/athlete-3-center.jpg" />
+    </Helmet>
+
+    <Button label="Equipment Rental" to="/rental" style="success" />
+    <Button label="CrossFit Online Competition" to="https://games.crossfit.com/" isExternal style="google" />
+    <Button label="Mobile App" to="/app" style="info"/>
+    <p>
+      <hr/>
+    </p>
+    <Button label="Book Trial Class" to="/trial-class" />
+    <Button label="Contact Us" to="/index.html#contact" />
+    <Button label="Schedule" to="/index.html#schedule" />
+    <Button label="Pricing" to="/index.html#pricing" />
+  </main>
+)
+
+export default LinksPage
