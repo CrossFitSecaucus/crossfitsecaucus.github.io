ff876b018ef8f6ba398d7808406edd8ceed59bea
Manuel Neuhauser <manuel@rokatu.com>
2020-04-16 13:13:23 -0400

    Update inventory

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -40,30 +40,44 @@ class RentalsPage extends React.Component {
     super(props);
     this.state = {
       rentalItems: [
-        {label: 'rings (2)', cost: 5},
+        // {label: 'rings (2)', cost: 5},
+        {heading: 'Kettlebells'},
         {label: '18 lbs kettlebell', cost: 5},
         {label: '26 lbs kettlebell', cost: 5},
         {label: '35 lbs kettlebell', cost: 5},
+        {label: '45 lbs kettlebell', cost: 5},
         {label: '53 lbs kettlebell', cost: 5},
+        {heading: 'Medicine Balls'},
         {label: '10 lbs med ball', cost: 5},
         {label: '12 lbs med ball', cost: 5},
         {label: '14 lbs med ball', cost: 5},
         {label: '16 lbs med ball', cost: 5},
         {label: '20 lbs med ball', cost: 5},
+        {heading: 'Plyo Boxes'},
         {label: '12" box', cost: 5},
         {label: '20" box', cost: 5},
         {label: '24" box', cost: 5},
+        {heading: 'Dumbbells'},
+        {label: '10 lbs dumbbell (1)', cost: 5, multiple: true},
+        {label: '15 lbs dumbbell (1)', cost: 5, multiple: true},
         {label: '20 lbs dumbbell (1)', cost: 5, multiple: true},
-        {label: '25 lbs dumbbell (1)', cost: 5, multiple: true},
+        // {label: '25 lbs dumbbell (1)', cost: 5, multiple: true},
+        {label: '30 lbs dumbbell (1)', cost: 10, multiple: true},
+        {label: '45 lbs dumbbell (1)', cost: 10, multiple: true},
+        {label: '50 lbs dumbbell (1)', cost: 10, multiple: false},
+        {label: '70 lbs dumbbell (1)', cost: 10, multiple: true},
+        {heading: 'Barbells & Plates'},
+        {label: '5 lbs plates (2)', cost: 5},
         {label: '15 lbs plates (2)', cost: 5},
         {label: '25 lbs plates (2)', cost: 10},
         {label: '35 lbs plates (2)', cost: 13},
         {label: '45 lbs plates (2)', cost: 15, multiple: true},
         {label: 'silencer mats (2)', cost: 10},
-        {label: 'barbell with clips', cost: 20},
+        {label: 'barbell (45 lbs) with clips', cost: 20},
         {label: 'squat rack', cost: 15},
         {label: 'bench', cost: 15},
-        {label: 'Concept2 rower', cost: 100},
+        {heading: 'Conditioning/Cardio'},
+        {label: 'Concept2 (PM5) rower', cost: 100},
         {label: 'Rogue Echo bike', cost: 100},
       ],
     }
@@ -148,17 +162,23 @@ class RentalsPage extends React.Component {
                         </thead>
                         <tbody>
                         {
-                          rentalItems.map(({label, cost, selected, quantity, multiple}, idx) => (
-                            <RentalItemRow
-                              key={idx}
-                              label={label}
-                              cost={cost}
-                              quantity={quantity || ''}
-                              selected={!!selected}
-                              multiple={multiple}
-                              onQuantityChange={this._handleQuantityChange(idx)}
-                              onSelectChange={this._handleSelectChange(idx)}
-                            />
+                          rentalItems.map((r, idx) => (
+                            r.heading ? (
+                              <tr key={idx}>
+                                <td colSpan="4"><h4><br/>{r.heading}</h4></td>
+                              </tr>
+                            ) : (
+                              <RentalItemRow
+                                key={idx}
+                                label={r.label}
+                                cost={r.cost}
+                                quantity={r.quantity || ''}
+                                selected={!!r.selected}
+                                multiple={r.multiple}
+                                onQuantityChange={this._handleQuantityChange(idx)}
+                                onSelectChange={this._handleSelectChange(idx)}
+                              />
+                            )
                           ))
                         }
                         </tbody>
