9ccf6e879f3ab538ec37a0dc1b40a7d9bc801baa
Manuel Neuhauser <manuel@rokatu.com>
2020-04-29 11:35:03 -0400

    Improve rentals page

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -40,45 +40,45 @@ class RentalsPage extends React.Component {
     super(props);
     this.state = {
       rentalItems: [
+        {heading: 'Barbells & Plates'},
+        {label: '5 lbs plates (2)', cost: 5, deposit: 20},
+        {label: '15 lbs plates (2)', cost: 7, deposit: 60},
+        {label: '25 lbs plates (2)', cost: 10, deposit: 100},
+        {label: '35 lbs plates (2)', cost: 13, deposit: 160},
+        {label: '45 lbs plates (2)', cost: 16, deposit: 180, multiple: true},
+        {label: 'silencer mats (2)', cost: 10, deposit: 150},
+        {label: 'Olympic barbell (45 lbs, 7 ft) with clips', cost: 25, deposit: 200},
+        {label: 'squat rack', cost: 15, deposit: 160},
+        {label: 'bench', cost: 15, deposit: 200},
+        {heading: 'Conditioning/Cardio'},
+        {label: 'Concept2 (PM5) rower', cost: 100, deposit: 400},
+        {label: 'Rogue Echo bike', cost: 100, deposit: 400},
         // {label: 'rings (2)', cost: 5},
         {heading: 'Kettlebells'},
-        {label: '18 lbs kettlebell', cost: 5},
-        {label: '26 lbs kettlebell', cost: 5},
-        {label: '35 lbs kettlebell', cost: 5},
-        {label: '45 lbs kettlebell', cost: 5},
-        {label: '53 lbs kettlebell', cost: 5},
-        {heading: 'Medicine Balls'},
-        {label: '10 lbs med ball', cost: 5},
-        {label: '12 lbs med ball', cost: 5},
-        {label: '14 lbs med ball', cost: 5},
-        {label: '16 lbs med ball', cost: 5},
-        {label: '20 lbs med ball', cost: 5},
-        {heading: 'Plyo Boxes'},
-        {label: '12" box', cost: 5},
-        {label: '20" box', cost: 5},
-        {label: '24" box', cost: 5},
-        {heading: 'Dumbbells'},
-        {label: '10 lbs dumbbell (1)', cost: 5, multiple: true},
-        {label: '15 lbs dumbbell (1)', cost: 5, multiple: true},
-        {label: '20 lbs dumbbell (1)', cost: 5, multiple: true},
+        {label: '18 lbs kettlebell', cost: 5, deposit: 40},
+        {label: '26 lbs kettlebell', cost: 5, deposit: 50},
+        // {label: '35 lbs kettlebell', cost: 5},
+        // {label: '45 lbs kettlebell', cost: 5},
+        {label: '53 lbs kettlebell', cost: 5, deposit: 80},
+        // {heading: 'Medicine Balls'},
+        // {label: '10 lbs med ball', cost: 5},
+        // {label: '12 lbs med ball', cost: 5},
+        // {label: '14 lbs med ball', cost: 5},
+        // {label: '16 lbs med ball', cost: 5},
+        // {label: '20 lbs med ball', cost: 5},
+        // {heading: 'Plyo Boxes'},
+        // {label: '12" box', cost: 5},
+        // {label: '20" box', cost: 5},
+        // {label: '24" box', cost: 10},
+        // {heading: 'Dumbbells'},
+        // {label: '10 lbs dumbbell (1)', cost: 5, multiple: true},
+        // {label: '15 lbs dumbbell (1)', cost: 5, multiple: true},
+        // {label: '20 lbs dumbbell (1)', cost: 5, multiple: true},
         // {label: '25 lbs dumbbell (1)', cost: 5, multiple: true},
-        {label: '30 lbs dumbbell (1)', cost: 10, multiple: true},
-        {label: '45 lbs dumbbell (1)', cost: 10, multiple: true},
-        {label: '50 lbs dumbbell (1)', cost: 10, multiple: false},
-        {label: '70 lbs dumbbell (1)', cost: 10, multiple: true},
-        {heading: 'Barbells & Plates'},
-        {label: '5 lbs plates (2)', cost: 5},
-        {label: '15 lbs plates (2)', cost: 5},
-        {label: '25 lbs plates (2)', cost: 10},
-        {label: '35 lbs plates (2)', cost: 13},
-        {label: '45 lbs plates (2)', cost: 15, multiple: true},
-        {label: 'silencer mats (2)', cost: 10},
-        {label: 'barbell (45 lbs) with clips', cost: 20},
-        {label: 'squat rack', cost: 15},
-        {label: 'bench', cost: 15},
-        {heading: 'Conditioning/Cardio'},
-        {label: 'Concept2 (PM5) rower', cost: 100},
-        {label: 'Rogue Echo bike', cost: 100},
+        // {label: '30 lbs dumbbell (1)', cost: 10, multiple: true},
+        // {label: '45 lbs dumbbell (1)', cost: 10, multiple: true},
+        // {label: '50 lbs dumbbell (1)', cost: 10, multiple: false},
+        // {label: '70 lbs dumbbell (1)', cost: 10, multiple: true},
       ],
     }
     this._handleSelectChange = this._handleSelectChange.bind(this);
@@ -117,11 +117,21 @@ class RentalsPage extends React.Component {
   render() {
     const {rentalItems} = this.state;
     let total = 0;
+    let deposit = 0;
     rentalItems.forEach(r => {
       if (r.selected) {
         total += r.cost * r.quantity;
+        if (r.deposit) {
+          deposit += r.deposit * r.quantity;
+        }
       }
     })
+
+    if (deposit > 200 && deposit < 500) {
+      deposit = 200;
+    } else if (deposit >= 500) {
+      deposit = 400;
+    }
 
     return (
       <main>
@@ -144,7 +154,7 @@ class RentalsPage extends React.Component {
                 <hr/>
                 <h3>Rental Overview</h3>
                 <ul>
-                  <li>Free home delivery within local area for rental of $50+/week</li>
+                  <li>Free home delivery within local area for rental of $100+/week</li>
                   <li>Rentals are week-to-week</li>
                   <li>Minimum rental period is 3 weeks</li>
                   <li>Equipment subject to availability. First come, first serve.</li>
@@ -183,6 +193,15 @@ class RentalsPage extends React.Component {
                         }
                         </tbody>
                     </table>
+                    {!total && (
+                      <div>
+                        <hr/>
+                        <h3 className="text-center">
+                          ☝️Select desired equipment from the list.
+                        </h3>
+                        <hr/>
+                      </div>
+                    )}
                     {total > 0 && (
                       <div>
                         <hr/>
@@ -191,13 +210,21 @@ class RentalsPage extends React.Component {
                         </h3>
                         <p className="text-center" style={{padding: 0}}>
                           <strong>
-                            {total >= 50 ? '🚚 Free home delivery' : '🏢 Pick-up Only'}
+                            {`($${deposit} deposit required.)`}
                           </strong>
                         </p>
-                        {total >= 150 && (
+                        <hr/>
+                        {total < 35 && (
                           <p className="text-center" style={{padding: 0}}>
                             <strong>
-                              {'📦 1 free case of 24 FitAid drinks ($72 value)'}
+                              {'⚠️ $35 minimum rental rate not met.'}
+                            </strong>
+                          </p>
+                        )}
+                        {total >= 35 && (
+                          <p className="text-center" style={{padding: 0}}>
+                            <strong>
+                              {total >= 100 ? '🚚 Free home delivery' : '🏢 Selection ready for pick-up weekdays at 6pm'}
                             </strong>
                           </p>
                         )}
@@ -206,13 +233,14 @@ class RentalsPage extends React.Component {
                     )}
 
                     <Contact
-                      messageAddition={rentalItems.filter(r => r.selected).map(r => `${r.quantity || 1}x ${r.label}`).join("\n")}
+                      messageAddition={rentalItems.filter(r => r.selected).map(r => `${r.quantity || 1}x ${r.label} ($${r.cost}/item)`).join("\n") + `\n\nWeekly Rate: $${total} / Deposit $${deposit}`}
                       noCard
                       header={
                         <div>
-                          <h3>Rental Request</h3>
-                          <p>After entering your contact information, please also include the delivery address, and the date and 2 hour time
-                          window during which we may deliver the equipment in the message field.</p>
+                          <h3>"How do I get my hands on the equipment?"</h3>
+                          <p><strong>Let us know you're interested by selecting the equipment you want to rent above and submitting the form below.</strong></p>
+                          <p>For even faster service, after completing the form please <a href="http://register.crossfit-secaucus.com/" target="_blank">create an account for yourself</a> on our website and add your credit card information. (You won't get charged until you agree to the rental terms.)</p>
+                          <p>You may pick up your equipment the same day (if your application is received before 3pm) or the next day at 6pm.</p>
                         </div>
                       }
                       submitLabel="Send Rental Request"
