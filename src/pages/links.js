8f2ee8ca50c192cfb4573ddeee1a2d23522fe1f7
Manuel Neuhauser <manuel@rokatu.com>
2022-04-23 20:53:13 -0400

    Add memorial day wod to link tree

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 import Link from 'gatsby-link'
+import moment from 'moment'
 
 const Button = ({label, to, isExternal = false, color = 'primary'}) => (
   <div className="row" style={{margin: 15, width: '100%'}}>
@@ -31,6 +32,13 @@ const LinksPage = () => (
       <meta property="og:url" content="https://crossfit-secaucus.com/links/" />
       <meta property="og:image" content="https://crossfit-secaucus.com/img/athlete-3-center.jpg" />
     </Helmet>
+
+    {moment().isBefore('2022-05-28') && (
+        <Button label="🇺🇸 Memorial Day WOD" to="https://clients.mindbodyonline.com/classic/ws?studioid=40911&stype=-7&sTG=31&sVT=27&sView=day&sLoc=0&date=05/28/22" isExternal  />
+    )}
+    {moment().isBefore('2022-05-28') && (
+        <Button label="👕 MDW T-shirt Pre-order" to="https://clients.mindbodyonline.com/classic/ws?studioid=40911&stype=43" isExternal color="info" />
+    )}
 
     <Button label="5-Class Trial" to="http://try.crossfit-secaucus.com/" isExternal color="danger" />
 
